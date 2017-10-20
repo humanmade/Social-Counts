392b427c495297153f48174c8c8e91d8e242e5af
Joe Hoyle <joehoyle@gmail.com>
2017-10-20 21:30:54 +0300

    Add expires to social counts.

--- social-counts.php
+++ social-counts.php
@@ -38,6 +38,12 @@ if ( ! defined( 'HM_SOCIAL_COUNTS_API_KEY' ) ) {
  */
 function get_total_share_count( int $post_id ) {
 	$count = get_post_meta( $post_id, 'hm_share_counts', true );
+	$date = get_post_meta( $post_id, 'hm_share_counts_updated_date', true );
+	$expires = apply_filters( 'hm_social_counts_expires_time', HOUR_IN_SECONDS );
+
+	if ( $date + $expires < time() ) {
+		return update_social_share_count( $post_id );
+	}
 
 	if ( ! empty( $count ) ) {
 		return $count;
@@ -47,17 +53,31 @@ function get_total_share_count( int $post_id ) {
 }
 
 function update_social_share_count( int $post_id ) {
+	update_post_meta( $post_id, 'hm_share_counts_updated_date', time() );
 	$url = get_permalink( $post_id );
-	$url = apply_filters( 'hm_social_counts_url_for_post_id', $url, $post_id );
-	$url = str_replace( site_url(), 'http://snopes.com', $url );
-	$response = wp_remote_get( esc_url_raw( 'http://free.sharedcount.com/?url=' . $url . '&apikey=' . HM_SOCIAL_COUNTS_API_KEY ) );
+	$urls = apply_filters( 'hm_social_counts_urls_for_post_id', [ $url ], $post_id );
+	$all_counts = [];
+	foreach ( $urls as $url ) {
+		$counts = get_counts_for_url( $url );
+		if ( is_wp_error( $counts ) ) {
+			continue;
+		}
+		foreach ( $counts as $key => $value ) {
+			$all_counts[ $key ] += $value;
+		}
+	}
+	update_post_meta( $post_id, 'hm_share_counts', $all_counts );
+	update_post_meta( $post_id, 'hm_total_share_count', $all_counts['total'] );
+	return $all_counts;
+}
+
+function get_counts_for_url( string $url ) {
+	$response = wp_remote_get( esc_url_raw( 'https://api.sharedcount.com/?url=' . $url . '&apikey=' . HM_SOCIAL_COUNTS_API_KEY ) );
 	$response = json_decode( wp_remote_retrieve_body( $response ), true );
 	if ( ! empty( $response['Error'] ) ) {
 		return new WP_Error( $response['Type'], $response['Error'] );
 	}
 	$response['Facebook'] = $response['Facebook']['total_count'];
 	$response['total'] = array_sum( $response );
-	update_post_meta( $post_id, 'hm_share_counts', $response );
-	update_post_meta( $post_id, 'hm_total_share_count', $response['total'] );
 	return $response;
 }
